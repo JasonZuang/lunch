e2ae9f8079202091d5d8ec8d3a6d8fa569e654b2
Jason Zuang <zuangjason@gmail.com>
2024-11-15 15:04:46 -0800

    react front-end with some css

---
+++ breakfast-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './components/HashBrown/HashBrown'
+import HashBrown from './components/HashBrown/HashBrown';
+
+function App() {
+  return (
+    <div className="App">
+    <HashBrown/>
+    </div>
+  );
+}
+
+export default App;
---
+++ breakfast-app/src/components/HashBrown/HashBrown.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import styles from './HashBrown.module.css';
+
+const HashBrown: React.FC = () => {
+  const [input, setInput] = useState('');
+  const [hashOutput, setHashOutput] = useState('');
+
+  const calcHash = async (value: string): Promise<string> => {
+    try {
+        console.log(value);
+        const response = await fetch('https://breakfastatjimmys.azurewebsites.net/hashbrown', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ data: value }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Not OK Response');
+      }
+
+      const hash: string = await response.text();
+      return hash;
+    } catch (error) {
+      console.error('Error in sending request to Jimmy', error);
+      return '';
+    }
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const inputElement = document.getElementById('inputData') as HTMLInputElement;
+    const inputText: string = inputElement.value;
+    const hash = await calcHash(inputText);
+    setInput(inputText);
+    setHashOutput(hash);
+    inputElement.value = '';
+
+    // Hide the response box after 1 minute (60000 milliseconds)
+    setTimeout(() => {
+      setHashOutput('');
+    }, 3000);
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(hashOutput).then(() => {
+      console.log('Copied to clipboard: ' + hashOutput);
+    }).catch((error) => {
+      console.error('Error in copying to clipboard', error);
+    });
+  };
+
+  return (
+    <div className={styles.hashbrown}>
+      <h1>Breakfast at Jimmy's</h1>
+      <form id="postForm" onSubmit={handleSubmit}>
+        <label>Order Number:</label>
+        <input type="text" id="inputData" name="data" required autoComplete="off" />
+        <button type="submit">Submit</button>
+      </form>
+      {hashOutput && (
+        <div id="response" className={styles.response}>
+          <h2>Response:</h2>
+          <p id="responseData">{hashOutput}</p>
+          <button id="copyButton" className={styles.copyButton} onClick={copyToClipboard}>Copy to Clipboard</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HashBrown;
